fdc0a9f4492d2162707135ace662c8c74065df45
Jonathan <join_mg@hotmail.com>
2019-10-18 16:43:57 -0500

    Login done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,21 @@
-class User < ApplicationRecord
-  before_save { email.downcase! }
-  validates(:name, presence: true, length: { maximum: 50 })
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates(:email, presence: true, length: { maximum: 255 },
+class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false })
+                    uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  # Returns the hash digest of the given string.
+  def self.digest(string)
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.cost
+    return BCrypt::Engine::MIN_COST if ActiveModel::SecurePassword.min_cost
+
+    BCrypt::Engine.cost
+  end
 end
